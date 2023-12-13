332ee5119a10e7b445567d20137de31c66f19e97
Deekshith-AC <85830510+Deekshith-AC@users.noreply.github.com>
2023-12-13 10:26:15 +0530

    Done

--- Week1/Day2/script.js
+++ Week1/Day2/script.js
@@ -1,21 +1,60 @@
-class Person{
-    constructor(name, age){
-        this.name = name
-        this.age = age
-    }
-     BirthDate = function(...n){
+// class Person{
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+//      BirthDate = function(...n){
 
-        let DateOfBirth = new Date(...n)
+//         let DateOfBirth = new Date(...n)
 
-       return DateOfBirth
+//        return DateOfBirth
         
         
-     }
-}
+//      }
+// }
 
-let p1 = new Person("Deekshi", 21);
+// let p1 = new Person("Deekshi", 21);
 
 
 
-let z = p1.BirthDate(2002,0,20,4, 35, 0)
-console.log(z) 
+// let z = p1.BirthDate(2002,0,20,4, 35, 0)
+// console.log(z) 
+
+
+
+//Function OverRiding
+// class Person{
+
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+
+//      BirthDate = function(...n){
+
+//         let DateOfBirth = new Date(...n)
+//        return DateOfBirth
+//      }
+// }
+
+// class Man extends Person{
+//     constructor(name, age, salary){
+//     super(name, age)
+//     this.salary = salary
+//     }
+
+//     displaySalary = function(n){
+//         console.log(n)
+//     }
+
+//     BirthDate = function(...m){
+//         let DateOfBirth = new Date(...m)
+//         return DateOfBirth
+//     }
+// }
+
+// let p1 = new Person("Deekshith", 21)
+// console.log(p1.BirthDate(2002, 0, 20, 5, 35, 0))
+// let m1 = new Man("Deekshith A C",21, 7500)
+// m1.displaySalary(m1.salary)
+// console.log(p1.BirthDate(2002, 0, 23, 5, 35, 0))
