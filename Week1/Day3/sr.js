d710fa124d754dd4ecf0640a89ce0243cd3ed810
Deekshith-AC <85830510+Deekshith-AC@users.noreply.github.com>
2023-12-13 15:29:40 +0530

    Done

--- Week1/Day3/sr.js
+++ Week1/Day3/sr.js
@@ -1,27 +1,3 @@
-// class Person{
-//     constructor(name, age){
-//         this.name = name
-//         this.age = age
-//     }
-//      BirthDate = function(...n){
-
-//         let DateOfBirth = new Date(...n)
-
-//        return DateOfBirth
-        
-        
-//      }
-// }
-
-// let p1 = new Person("Deekshi", 21);
-
-
-
-// let z = p1.BirthDate(2002,0,20,4, 35, 0)
-// console.log(z) 
- 
-
-
 //Function OverRiding
 // class Person{
 
