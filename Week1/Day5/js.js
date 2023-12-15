e298c540244f2d74f6be0f1873dc409fe94da895
Deekshith-AC <85830510+Deekshith-AC@users.noreply.github.com>
2023-12-15 14:55:32 +0530

    Done

--- Week1/Day5/js.js
+++ Week1/Day5/js.js
@@ -1,6 +1,6 @@
-let d1  = document.getElementById('d1')
-let d2  = document.getElementById('d2')
-let d3  = document.getElementById('d3')
+// let d1  = document.getElementById('d1')
+// let d2  = document.getElementById('d2')
+// let d3  = document.getElementById('d3')
 
 // d1.onclick = () => {
 // console.log("D1 got Clicked");
@@ -16,21 +16,46 @@ let d3  = document.getElementById('d3')
 //     console.log("D3 got Clicked");
 //     }
 
-d1.addEventListener('click',d1fun,true)
-d2.addEventListener('click',d2fun,true)
-d3.addEventListener('click',d3fun)
+// d1.addEventListener('click',d1fun,true)
+// d2.addEventListener('click',d2fun,true)
+// d3.addEventListener('click',d3fun)
 
-function d1fun()
-{
-    console.log("D1 got Clicked");
-}
+// function d1fun()
+// {
+//     console.log("D1 got Clicked");
+// }
 
-function d2fun()
-{
-    console.log("D2 got Clicked");
-}
+// function d2fun()
+// {
+//     console.log("D2 got Clicked");
+// }
 
-function d3fun()
-{
-    console.log("D3 got Clicked");
-}
+// function d3fun()
+// {
+//     console.log("D3 got Clicked");
+// }
+
+
+
+
+
+
+// function ajaxRequest(){
+//     let dataXHR = new XMLHttpRequest();
+//     dataXHR.open('GET', "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=ftchyox0mKaTDgR85FFni6sbI82W70KqXVbhDslY", true)
+//     dataXHR.send()
+//     dataXHR.onprogress = function () {console.log("On Progress");}
+//     dataXHR.onload = function () {console.log(JSON.parse(this.responseText))}
+    
+// }
+// ajaxRequest()
+
+
+
+
+
+
+
+
+
+
