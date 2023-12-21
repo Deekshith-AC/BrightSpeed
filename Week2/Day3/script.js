2d78ceece431e5be4d510326fbbb07d910b88c63
Deekshith-AC <85830510+Deekshith-AC@users.noreply.github.com>
2023-12-21 12:34:18 +0530

    Done

--- Week2/Day3/script.js
+++ Week2/Day3/script.js
@@ -1,4 +1,8 @@
 var app = angular.module("myApp", []);
-app.controller("myCtrl", [$scope,function(s){
-    
+app.controller("myCtrl", [ "$scope",function(s){
+    s.currentDate = new Date
+
+
+    s.arr = ["abc", "pqr","def", "ghi", "jkl", "mno"]
+
 }])
